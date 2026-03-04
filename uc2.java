75d932dea6c1cbcb3d8539a4c8724abcc8a1b9a8
lokesh <vj4557@srmist.edu.in>
2026-03-04 14:08:33 +0530

    added uc2

---
+++ uc2.java
@@ -0,0 +1,13 @@
+class uc2 {
+    public static void main(String[] args) {
+        System.out.println("   ***   "+"    ***   "+" ******   "+"    *****");
+        System.out.println(" **   ** "+"  **   ** "+" **    **  "+" **");
+        System.out.println("**     **"+" **     **"+" **     **"+" **");
+        System.out.println("**     **"+" **     **"+" **    ** "+"  **");
+        System.out.println("**     **"+" **     **"+" ******   "+"    ***");
+        System.out.println("**     **"+" **     **"+" **       "+"       **");
+        System.out.println("**     **"+" **     **"+" **       "+"         **");
+        System.out.println(" **   ** "+"  **   ** "+" **       "+"        **");
+        System.out.println("   ***   "+"    ***  "+"  **       "+"   *****");
+    }
+}
